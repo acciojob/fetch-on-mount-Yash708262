fcdbc30ccb8fb17945f3d772b487295ae1dbe192
Yash708262 <yash708262@gmail.com>
2025-09-20 15:51:21 +0530

    cefejh

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,33 @@
-
-import React from "react";
+import "regenerator-runtime/runtime";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const [data,setData] = useState([]);
+
+  useEffect(() => {
+     const fetchData = async () => {
+      try {
+        const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+        const json = await res.json();
+        setData(json);
+      } catch (err) {
+        console.error("Error fetching data:", err);
+      }
+    };
+
+    fetchData();
+  }, [])
+
   return (
     <div>
-        {/* Do not remove the main div */}
+        {data.map((item) => (
+        <div key={item.id} style={{border: "1px solid #ccc", margin: "10px", padding: "10px"}}>
+          <h2>{item.title}</h2>
+          <p>{item.body}</p>
+        </div>
+      ))}
     </div>
   )
 }
